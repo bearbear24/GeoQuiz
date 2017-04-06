b5a860a27c18384f8327a131cb9612551929f19b
Chunyan Wang <cywang24@gmail.com>
2017-04-05 20:52:06 -0700

    Send data between the QuizActivity and CheatActivity.

--- app/src/main/java/com/chunyanwang/geoquiz/CheatActivity.java
+++ app/src/main/java/com/chunyanwang/geoquiz/CheatActivity.java
@@ -1,25 +1,51 @@
 package com.chunyanwang.geoquiz;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class CheatActivity extends AppCompatActivity {
 
+    private static final String EXTRA_ANSWER_IS_TRUE =
+            "com.chunyanwang.geoquiz.QuizActivity.answer_is_true";
+    private static final String EXTRA_ANSWER_SHOWN =
+            "com.chunyanwang.geoquiz.QuizActivity.answer_shown";
+
     private Button showCheatButton;
+    private TextView answerText;
+    private boolean answerIsTrue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cheat);
 
+        answerIsTrue = getIntent().getBooleanExtra(EXTRA_ANSWER_IS_TRUE, false);
         showCheatButton = (Button) findViewById(R.id.show_cheat_button);
+        answerText = (TextView) findViewById(R.id.answer_text);
         showCheatButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                if (answerIsTrue) {
+                    answerText.setText(R.string.true_button);
+                } else {
+                    answerText.setText(R.string.false_button);
+                }
+                setAnswerShownResult(true);
             }
         });
+    }
+
+    public static boolean wasAnswerShown(Intent data) {
+        return data.getBooleanExtra(EXTRA_ANSWER_SHOWN, false);
+    }
+
+    private void setAnswerShownResult(boolean isAnswerShown) {
+        Intent data = new Intent();
+        data.putExtra(EXTRA_ANSWER_SHOWN, isAnswerShown);
+        setResult(RESULT_OK, data);
     }
 }
--- app/src/main/java/com/chunyanwang/geoquiz/QuizActivity.java
+++ app/src/main/java/com/chunyanwang/geoquiz/QuizActivity.java
@@ -1,5 +1,6 @@
 package com.chunyanwang.geoquiz;
 
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -11,6 +12,9 @@ import android.widget.Toast;
 public class QuizActivity extends AppCompatActivity {
 
     private static final String QUESTION_TEXT_INDEX_KEY = "QUESTION_TEXT_INDEX";
+    private static final String EXTRA_ANSWER_IS_TRUE =
+            "com.chunyanwang.geoquiz.QuizActivity.answer_is_true";
+    private static final int REQUEST_CODE = 0;
     private Button trueButton;
     private Button falseButton;
     private Button nextButton;
@@ -18,12 +22,19 @@ public class QuizActivity extends AppCompatActivity {
 
     private TextView questionTextView;
     private int currentViewIndex;
+    private boolean isCheater;
 
     private Question[] questionBank = new Question[] {
             new Question(R.string.question_ocean, true),
             new Question(R.string.question_africa, false),
             new Question(R.string.question_mideast, true),
     };
+
+    public static Intent newIntent(Context packageContext, boolean answerIsTrue) {
+        Intent i = new Intent(packageContext, CheatActivity.class);
+        i.putExtra(EXTRA_ANSWER_IS_TRUE, answerIsTrue);
+        return i;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -69,10 +80,24 @@ public class QuizActivity extends AppCompatActivity {
         cheatButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent i =  new Intent(QuizActivity.this, CheatActivity.class);
-                startActivity(i);
+                boolean answerIsTrue = questionBank[currentViewIndex].getAnswerTrue();
+                Intent i =  newIntent(QuizActivity.this, answerIsTrue);
+                startActivityForResult(i, REQUEST_CODE);
             }
         });
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode != RESULT_OK) {
+            return;
+        }
+        if (requestCode == REQUEST_CODE) {
+            if (data == null) {
+                return;
+            }
+        }
+        isCheater = CheatActivity.wasAnswerShown(data);
     }
 
     private void updateQuestion() {
